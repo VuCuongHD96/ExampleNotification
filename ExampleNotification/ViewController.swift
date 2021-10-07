fea6794ab0f4ba05d2f68e41ef6235c4788726bd
CuongVx <sun@Suns-Mac-mini.local>
2021-10-07 17:16:57 +0700

    Local Notification

--- ExampleNotification/ViewController.swift
+++ ExampleNotification/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            print("------ debug Granted = \(granted)")
+        }
+        center.delegate = self
+        
+        let content = UNMutableNotificationContent()
+        content.title = "This is title content"
+        content.body = "This is body"
+         
+        let date = Date().addingTimeInterval(5)
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString,
+                                            content: content,
+                                            trigger: trigger)
+        
+        center.add(request) { error in
+            print("------ debug Error = \(error)")
+        }
     }
-
-
 }
 
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .list, .sound])
+    }
+}
