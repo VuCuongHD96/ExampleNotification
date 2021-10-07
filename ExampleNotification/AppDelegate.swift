e8b253a4b8af10b679869290cde724d39b40b26d
CuongVx <sun@Suns-Mac-mini.local>
2021-10-07 14:55:26 +0700

    split notif delegate

--- ExampleNotification/AppDelegate.swift
+++ ExampleNotification/AppDelegate.swift
@@ -11,11 +11,16 @@ import  UserNotifications
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    let myRemoteNotification = MyRemoteNotification()
+    let myNotification = MyNotification()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        MyLocalNotification.registerNotification()
-        myRemoteNotification.registerForPushNotifications()
+        myNotification.registerForPushNotifications()
+        showNotif()
         return true
+    }
+    
+    private func showNotif() {
+        let content = NotificationContent(title: "aaaa", body: "bbbb")
+        myNotification.pushNotif(with: content, after: 2)
     }
 }
